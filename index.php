02b112c4d83aeefd5ac2903727b25197e0aa33e7
freeburma <kophyoblue@gmail.com>
2021-07-06 16:19:35 +1200

    01_Init_Template

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+    Plugin Name: Product Custom Table 
+    Description: Product with Custom Table. We will be developing a wordpress plugin 
+    with Google Line Chart.
+    Version: 1.0
+    Author: Your Name 
+    License: GPLv2 or Later
+    
+    Text Domain: https://stackoverflow.com/questions/40867316/call-to-undefined-function-convert-to-screen
+
+    **** Coding Ref: https://github.com/Veraxus/wp-list-table-example.git 
+
+*/
+
+
+?>
