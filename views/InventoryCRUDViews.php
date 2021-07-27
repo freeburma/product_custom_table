fe137445d089146b800fde05527fb5a81394654a
freeburma <kophyoblue@gmail.com>
2021-07-27 20:47:42 +1200

    [DONE] Updating and Deleting

--- views/InventoryCRUDViews.php
+++ views/InventoryCRUDViews.php
@@ -2,16 +2,18 @@
     $id = null; 
     $productTitle = null; 
     $productDescription = null; 
+    $target_file = null; 
 
-    // $dirName = "imgs"; 
-    // $productFileBase = get_template_directory() ; 
-    // $productFilePath = $productFileBase . "/" . $dirName . "/"; 
+    $dirName = "imgs"; 
+    $productFileBase = get_template_directory() ; 
+    $productFilePath = $productFileBase . "/" . $dirName . "/"; 
+    // echo $productFilePath; 
 
-    // if ( ! file_exists($productFilePath))
-    // {
-    //     mkdir($productFilePath); 
+    if ( ! file_exists($productFilePath))
+    {
+        mkdir($productFilePath); 
         
-    // }// end if 
+    }// end if 
 
     // //// Getting theme path and url
     // echo "get_template_directory_uri : ". get_template_directory_uri() . "<br />"; 
@@ -57,16 +59,63 @@
         {
             if (wp_verify_nonce( $nounce, 'inventory_nonce' ))
             {
-                
+                $productImageName_1_Old = null; 
+                $productImageName_1_Old = $productImageName_1; 
+
+                $productTitle = null; 
+                $productDescription = null; 
+                $productImageName_1 = null; 
+
+
+
+                $productTitle = sanitize_text_field( $_POST["title"] ); 
+                $productDescription = sanitize_text_field( $_POST["description"] ); 
+                // $productImageName_1 = sanitize_text_field( $_POST["photo_1"] ); 
+                $productImageName_1 = sanitize_text_field( $_FILES["photo_1"]["name"]); 
+
+                //// 1. Update the new info in db 
+                $inventoryQuery = array(
+                    'Title' => $productTitle, 
+                    'ProductDescription' => $productDescription, 
+                    'FilePath' => $dirName, 
+                    'ImageName_1' => $productImageName_1, 
+                    'StoreDate' => $productStoreDate
+
+                ); 
+
+                //// 2. Add the new picture 
+                $wpdb->update('wp_custom_inventory', $inventoryQuery, array('Id' => $getId)); 
+
+                //// 3. Delete Old picture 
+                $target_file = $productFilePath . $productImageName_1_Old;
+
+                RemoveFile($target_file); 
             }// end if
+
+            print('<script>window.location.href="admin.php?page=tutorial_product"</script>');
+
+
         }// end if 
 
         if (isset($_POST["DeletingProduct"]))
         {
             if (wp_verify_nonce( $nounce, 'inventory_nonce' ))
             {
-                
+                //// 1. Remove the data from db 
+                $wpdb->delete('wp_custom_inventory',array('Id' => $getId)); 
+
+                //// 2. Delete the picture 
+                $productImageName_1_Old = null; 
+                $productImageName_1_Old = $productImageName_1; 
+
+                $target_file = $productFilePath . $productImageName_1_Old;
+
+                RemoveFile($target_file); 
+
             }// end if
+
+            print('<script>window.location.href="admin.php?page=tutorial_product"</script>');
+
         }// end if 
         
         
@@ -98,77 +147,91 @@
                 $wpdb->query($productQuery); 
 
                 //// ============ Write the photo to a file =============
-                //// Ref: https://www.w3schools.com/php/php_file_upload.asp
+                // //// Ref: https://www.w3schools.com/php/php_file_upload.asp
                 $target_file = $productFilePath . basename($_FILES["photo_1"]["name"]);
-                $uploadOk = 1;
-                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-                // Check if image file is a actual image or fake image
-                if (isset($_POST["submit"]))
-                {
-                    $check = getimagesize($_FILES["photo_1"]["tmp_name"]);
-                    if ($check !== false) 
-                    {
-                        echo "File is an image - " . $check["mime"] . ".";
-                        $uploadOk = 1;
-                    } else 
-                    {
-                        echo "File is not an image.";
-                        $uploadOk = 0;
-                    }
-                }
-
-                // Check if file already exists
-                if (file_exists($target_file)) 
-                {
-                    echo "Sorry, file already exists.";
-                    $uploadOk = 0;
-                }
-
-                // Check file size - Change the Max_Post_Size in "php.ini"
-                if ($_FILES["photo_1"]["size"] > 128000000) 
-                {
-                    echo "Sorry, your file is too large.";
-                    $uploadOk = 0;
-                }
-
-                // Allow certain file formats
-                if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "gif") 
-                {
-                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                    $uploadOk = 0;
-                }
-
-                // Check if $uploadOk is set to 0 by an error
-                if ($uploadOk == 0) 
-                {
-                    echo "Sorry, your file was not uploaded.";
-                // if everything is ok, try to upload file
-                } 
-                else 
-                {
-                    if (move_uploaded_file($_FILES["photo_1"]["tmp_name"], $target_file)) 
-                    {
-                        echo "The file " . htmlspecialchars(basename($_FILES["photo_1"]["name"])) . " has been uploaded.";
-                    } 
-                    else 
-                    {
-                        echo "Sorry, there was an error uploading your file.";
-                    }
-                }
-
-               
+                AddFile($target_file); 
 
 
-            }// end if 
+            }// end if -> (wp_verify_nonce( $nounce, 'inventory_nonce' ))
 
             //// Redirecting to this page
-            print('<script>window.location.href="admin.php?page=tutorial_product_crud"</script>');
+            print('<script>window.location.href="admin.php?page=tutorial_product"</script>');
 
         }// end if 
 
     }// end if 
+
+    function AddFile($target_file)
+    {
+        //// Ref: https://www.w3schools.com/php/php_file_upload.asp
+        // $target_file = $productFilePath . basename($_FILES["photo_1"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["submit"]))
+        {
+            $check = getimagesize($_FILES["photo_1"]["tmp_name"]);
+            if ($check !== false) 
+            {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } 
+            else 
+            {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }// end if -> $check
+        }// end if -> (isset($_POST["submit"]))
+
+        // Check if file already exists
+        if (file_exists($target_file)) 
+        {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }// end if -> (file_exists($target_file)) 
+
+        // Check file size - Change the Max_Post_Size in "php.ini"
+        if ($_FILES["photo_1"]["size"] > 128000000) // 128 Mb
+        {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }// end if -> ($_FILES["photo_1"]["size"] > 128000000) // 128 Mb
+
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+            ) 
+        {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }// end if 
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) 
+        {
+            echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } // end if 
+        else 
+        {
+            if (move_uploaded_file($_FILES["photo_1"]["tmp_name"], $target_file)) 
+            {
+                echo "The file " . htmlspecialchars(basename($_FILES["photo_1"]["name"])) . " has been uploaded.";
+            } 
+            else 
+            {
+                echo "Sorry, there was an error uploading your file.";
+            }// end if 
+
+        }// end if -> ($uploadOk == 0) 
+    }// end AddFile
+
+    function RemoveFile($filePath)
+    {
+        wp_delete_file($filePath); 
+
+    }// end RemoveFile() 
 
 ?>
 <div class="container">
