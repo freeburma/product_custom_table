0751c5f979a6608068c53e12024a6114047a0202
freeburma <kophyoblue@gmail.com>
2021-08-27 20:03:47 +1200

    Add/Drop Table after plugin activating/deactivating

--- include/ProductPluginActivate.php
+++ include/ProductPluginActivate.php
@@ -4,6 +4,21 @@ class ProductPluginActivate
 {
     public static function activate()
     {
+        global $wpdb; 
+
+        $createTable = $wpdb->prepare("CREATE TABLE IF NOT EXISTS wp_custom_inventory 
+                                        (
+                                            Id int NOT NULL AUTO_INCREMENT primary key, 
+                                            Title TEXT, 
+                                            ProductDescription TEXT, 
+                                            FilePath TEXT,
+                                            ImageName_1 TEXT,
+                                            StoreDate DATETIME
+                                        )"
+                                    ); 
+
+        $wpdb->query($createTable);                                     
+
         flush_rewrite_rules( ); 
     }// end  activate()
 
--- include/ProductPluginDeactivate.php
+++ include/ProductPluginDeactivate.php
@@ -4,6 +4,12 @@ class ProductPluginDeactivate
 {
     public static function deactivate()
     {
+        global $wpdb; 
+
+        $deleteTable = "DROP TABLE IF EXISTS wp_custom_inventory"; 
+
+        $wpdb->query($deleteTable); 
+
         flush_rewrite_rules( ); 
     }// end  activate()
 
